258c322a4197bc4ccdd34623ed32bb7eeeb5bde6
rohezal <r.kratou@tu-bs.de>
2023-08-22 17:37:46 +0200

    added fileid string to pickel filename

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def loadCSV(image_folder, output_folder, image_height, image_width,fileid=""):
         print("There is no best element in metadata file. Skipping the file " + filename)
         return
 
-    with open(output_folder+"/"+filename_escaped+fileid+".pkl", "wb") as f:
+    with open(output_folder+"/"+filename_escaped+"_fileid_"+fileid+".pkl", "wb") as f:
         print(filename + ": " + fileid + ":" + str(best_element))
 
         shutil.copyfile(image_folder+"/"+best_element["id"]+".png", output_folder+"/"+filename_escaped+"fileid_"+fileid+"_"+"bestimage_"+best_element["id"]+".png")
